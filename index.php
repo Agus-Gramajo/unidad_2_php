426fc19fbeb36aee1594050dbebe53a5e57357e0
Agustina Gramajo <agus.gramajodw@gmail.com>
2021-07-01 15:42:09 -0300

    Creacion repo unidad 2 codo a codo php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = 'Agustina';
+$apellido = 'Gramajo';
+$edad = 34;
+$hobbie = 'Tomar vino';
+$editor = "Visual Studio Code";
+$sisema = 'Windows';
+
+
+echo 'Mi nombre es ' . $nombre . ' ' . $apellido . ' y tengo ' . $edad . ' años' . '. ' . '<br>Mi hobbie es ' . $hobbie . '. <br>' . 'El editor de codigo que utilizo es ' . $editor . ' y lo hago desde ' . $sisema;
